5ef43bb8902dc45aca4a63d348a6604824f674ce
Yinli <jiangyinli0803@gmail.com>
2025-04-23 10:13:15 +0200

    initial commit

---
+++ src/test/java/org/example/recap_todo_backend/RecapToDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recap_todo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapToDoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
