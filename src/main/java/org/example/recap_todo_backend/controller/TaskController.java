aeb134a9874a3b2664937feac8d1610baad76264
Yinli <jiangyinli0803@gmail.com>
2025-04-23 12:24:47 +0200

    erstellen Get/Post/Put/Delete Methoden in Service und Controller

--- src/main/java/org/example/recap_todo_backend/controller/TaskController.java
+++ src/main/java/org/example/recap_todo_backend/controller/TaskController.java
@@ -1,11 +1,45 @@
 package org.example.recap_todo_backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.recap_todo_backend.dto.TaskDto;
+import org.example.recap_todo_backend.exceptions.TaskNotFoundException;
+import org.example.recap_todo_backend.model.Task;
+import org.example.recap_todo_backend.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
 public class TaskController {
 
+    private final TaskService service;
 
+    public TaskController(TaskService taskService) {
+        this.service = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return service.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable String id) throws TaskNotFoundException {
+        return service.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody TaskDto taskDto) {
+        return service.addTask(taskDto);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable String id, @RequestBody Task newtTask) throws TaskNotFoundException {
+        return service.updateTask(id, newtTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable String id) throws TaskNotFoundException {
+         service.deleteTask(id);
+    }
 }
--- src/main/java/org/example/recap_todo_backend/service/TaskService.java
+++ src/main/java/org/example/recap_todo_backend/service/TaskService.java
@@ -1,7 +1,53 @@
 package org.example.recap_todo_backend.service;
 
+import org.example.recap_todo_backend.Repository.TaskRepository;
+import org.example.recap_todo_backend.dto.TaskDto;
+import org.example.recap_todo_backend.exceptions.TaskNotFoundException;
+import org.example.recap_todo_backend.model.Task;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+
+    private final TaskRepository repo;
+    private final IdService idService;
+    public TaskService(TaskRepository repo, IdService idService) {
+        this.repo = repo;
+        this.idService = idService;
+    }
+
+    public List<Task> getAllTasks() {
+        return repo.findAll();
+    }
+
+    public Task getTaskById(String id) throws TaskNotFoundException {
+        return repo.findById(id).orElseThrow(() -> new TaskNotFoundException("Task with ID: " + id + " not found."));
+    }
+
+    public Task addTask(TaskDto taskDto) {
+        Task newTask = new Task(
+                idService.randomId(),
+                taskDto.title(),
+                taskDto.status(),
+                taskDto.description()
+        );
+        repo.save(newTask);
+        return newTask;
+    }
+
+    public Task updateTask(String id, Task newTask) throws TaskNotFoundException {
+        Task oldTask = repo.findById(id).orElseThrow(()->new TaskNotFoundException("Task with ID: " + id + " not found."));
+        return repo.save(oldTask.withTitle(newTask.title())
+                .withStatus(newTask.status())
+                .withDescription(newTask.description()));
+       }
+
+    public void deleteTask(String id) throws TaskNotFoundException {
+       if(!repo.existsById(id)) {
+           throw new TaskNotFoundException("Task with ID: " + id + " not found.");
+       }
+       repo.deleteById(id);
+    }
 }
